50615651db97235099603be10248129ce97b7bb3
benneaux <benajamin.g.moran@gmail.com>
2017-09-27 08:22:12 +1000

    Attempted to rewrite first post as a md document

--- renderfiles.R
+++ renderfiles.R
@@ -2,6 +2,6 @@ library(rmarkdown)
 library(stringi)
 render("R/TestReport/HonoursMeeting.Rmd",
        output_dir = "_posts/", 
-       output_file = "2017-09-22-TestReport.md")
+       output_file = "2017-09-22-TestReport.html")
 
     
