2a46e26f6a146234ddea715459859177853f304c
benneaux <benajamin.g.moran@gmail.com>
2017-09-29 20:01:24 +1000

    Actual work this time!

--- renderfiles.R
+++ renderfiles.R
@@ -3,6 +3,6 @@ library(stringi)
 library(knitr)
 rmarkdown::render(file.choose(),
        output_dir = "_posts/",
-       output_file = paste(Sys.Date(),"TestBlog.html", sep = "-"),
+       output_file = paste(Sys.Date(),"DeJongPenzerAlgorithm.html", sep = "-"),
        output_format = render_jekyll())
 
